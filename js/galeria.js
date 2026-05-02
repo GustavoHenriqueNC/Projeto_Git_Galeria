2acde6c30d112f096b54ac02aff75ba13f473be4
lucasmaranhao2 <lucasmaranhao150@gmail.com>
2026-05-02 20:53:58 -0300

    feat: adicionar pop up de imagem ao clicar

--- js/galeria.js
+++ js/galeria.js
@@ -1,8 +1,9 @@
-// Zoom na imagem ao passar o mouse
-
-const itens = document.querySelectorAll(".galeria .galeria-item")
+const itens = document.querySelectorAll(".galeria-item")
 
 itens.forEach(function(item) {
+
+    // Zoom na imagem ao passar o mouse
+
     item.addEventListener("mouseenter", function(){
         item.style.scale= "1.3";
         item.style.zIndex = "1";
@@ -13,4 +14,33 @@ itens.forEach(function(item) {
         item.style.scale = "1.0";
         item.style.zIndex = "auto";
     });
+
+    // Pop-up após clicar na imagem
+
+    const popup = document.querySelector("#popup");
+    const page = document.querySelector(".galeria");
+
+    item.addEventListener("click", function() {
+        const full_img = item.querySelector("img").getAttribute("src");
+
+        page.style.opacity = "0.2";
+        page.style.transitionDuration = "0.4s";
+        page.style.position = "relative";
+        
+        popup.querySelector("img").setAttribute("src", full_img);
+        popup.removeAttribute("hidden");
+        popup.style.transitionDuration = "0.4s";
+        popup.style.zIndex = "1";
+    });
+
+    // Fechar pop-up
+    const close= document.querySelector("#close");
+
+    close.addEventListener("click", function() {
+        popup.setAttribute("hidden", "");
+        page.style.opacity = "1.0";
+        
+    })
+
+
 });
